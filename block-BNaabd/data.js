351e71626887e63ea5bf38923e0abcb4f5436b2a
Sushil Ghate <sushilsg30@gmail.com>
2023-01-15 12:17:57 +0530

    block-BNaabd

--- block-BNaabd/data.js
+++ block-BNaabd/data.js
@@ -1,24 +1,28 @@
-let http = require('http');
+let http = require("http");
+let qs = require("querystring");
 
 let server = http.createServer(handleRequest);
 
-
-
 function handleRequest(req, rsp) {
-    let dataFormat = req.headers['content-type'];
-    let store = '';
-    req.on('data', (chunk) => {
-        store = store + chunk;
-    })
+  let store = "";
+  req.on("data", (chunk) => {
+    store = store + chunk;
+  });
 
-    req.on('end', () => {
-        if(dataFormat === "application/json") {
-            let parsedData = JSON.parse(store);
-            resizeBy.end(parsedData);
-        }
-    })
+  req.on("end", () => {
+    if (req.method === "POST" && req.url === "/json") {
+      console.log(store);
+      resizeBy.setHeader("Content-Type", "application/json");
+      res.end(store);
+    }
+    if (req.method === "POST" && req.url === "/form") {
+      console.log(store);
+      let formData = qs.parse(store);
+      res.end(JSON.stringify(formData));
+    }
+  });
 }
 
-server.listen(7000, () =>{
-    console.log('Server listening on Server 7K');
+server.listen(7000, () => {
+  console.log("Server listening on Server 7K");
 });
